c19aef06056442c1437eb47a1bec3ceb627101f4
Lorena <gldiaz091@misena.edu.co>
2015-06-04 23:34:22 +0200

    cambio2

---
+++ Capitulo4/ejer1/log.php
@@ -0,0 +1,15 @@
+<?php
+$utc = date('U');
+$anio = date('Y');
+$mes = date('m');
+$dia = date('d');
+$hora = date('H');
+$minuto = date('i');
+$segundo = date('s');
+
+$usuariolog = $_SESSION['usuario'];
+
+
+
+
+?>
